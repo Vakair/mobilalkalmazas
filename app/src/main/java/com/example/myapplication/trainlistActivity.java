8b8318a71186bac04c60714f46150a22dcb40011
Káczán-Tóth Vajk <h373697@stud.u-szeged.hu>
2025-04-14 12:59:34 +0200

    commit
    commit

---
+++ app/src/main/java/com/example/myapplication/trainlistActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+
+public class trainlistActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = trainlistActivity.class.getName();
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_trainlist);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            Log.d(LOG_TAG, "Authenticated user!");
+        }else {
+            Log.d(LOG_TAG, "Unauthenticated user!");
+            finish();
+        }
+    }
+}
